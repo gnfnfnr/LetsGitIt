6ce4d650bc3ebf9819fbe359ee5ee89e823210f0
hyeonah <hyeona0000@naver.com>
2023-04-04 23:25:27 +0900

    first commit

---
+++ src/test/java/com/proj/letsgitit/LetsGitItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proj.letsgitit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetsGitItApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
