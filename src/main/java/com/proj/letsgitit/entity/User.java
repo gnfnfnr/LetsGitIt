443a365e435dee1de2769e8c5b4bb55e0695999a
ehgmlKim <dohee930@gmail.com>
2023-04-06 23:29:30 +0900

    feat : user entity 추가

--- src/main/java/com/proj/letsgitit/entity/User.java
+++ src/main/java/com/proj/letsgitit/entity/User.java
@@ -14,16 +14,16 @@ public class User {
     @Column(name="u_id")
     int uId;
 
-    @Column(name="login")
+    @Column(name="login")   //이게 깃에서 id를 가져올 때 객체명
     String login;
 
     @Column(name="name")
-    String name; //근데 이건 무슨 컬럼이지..? 일단 노션에 써있어서 넣음
+    String name; //근데 이건 무슨 컬럼이지..? 일단 노션에 써있어서 넣음 -> 깃에서 레포 이름 가져올 때 객체명
 
-    @Column(name="id")
+    @Column(name="id")  //이건 깃이 부여한 id...? 아마도
     String id;
 
-    @Column(name="html_url")
+    @Column(name="html_url") //유저 깃 주소
     String htmlUrl;
 
     @Column(name="blog")
@@ -35,5 +35,17 @@ public class User {
     @Column(name="email")
     String email;
 
+    @Column(name="school")
+    String school;
 
+    @Column(name="major")
+    String major;
+
+    @Column(name="graduate")
+    String graduate;
+
+    //플랫폼 어케 받기로 했는지 기억이 안난다...
+
+    @Column(name="intro") //소개글
+    String intro;
 }
