241ebec6902329d9adb0c9eccf678bd1ce7123ff
hyeonah <hyeona0000@naver.com>
2023-04-10 11:20:25 +0900

    feat: application.properties 수정

--- src/main/java/com/proj/letsgitit/Controller/GithubController.java
+++ src/main/java/com/proj/letsgitit/Controller/GithubController.java
@@ -4,14 +4,9 @@ package com.proj.letsgitit.Controller;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class GithubController {
-    @Value("${clientId}")
-    String clientId;
 
-    @GetMapping("/login")
-    public String home(){
-        return "https://github.com/login/oauth/authorize?client_id=" + clientId+ "&redirect_uri=http://localhost:9000/login/oauth2";
-    }
 }
