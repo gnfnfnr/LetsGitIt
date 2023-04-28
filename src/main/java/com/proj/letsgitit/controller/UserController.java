94ab3f0eed1ba71c27bb644891431ccda8485728
hyeonah <hyeona0000@naver.com>
2023-04-28 12:01:56 +0900

    fix: github login - db 저장 성공

--- src/main/java/com/proj/letsgitit/controller/UserController.java
+++ src/main/java/com/proj/letsgitit/controller/UserController.java
@@ -4,17 +4,39 @@ import com.proj.letsgitit.entity.User;
 import org.kohsuke.github.GHUser;
 import org.kohsuke.github.GitHub;
 import org.kohsuke.github.GitHubBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @Controller
 public class UserController {
+
+    @Value("${githubApiUrl}")
+    private String githubApiUrl;
+
+    @Value("${githubRedirectUrl}")
+    private String githubRedirectUrl;
+
+    @Value("${spring.security.oauth2.client.registration.github.client-id}")
+    private String clientId;
+
+    @Value("${spring.security.oauth2.client.registration.github.client-secret}")
+    private String clientSecret;
+
+    @GetMapping("/login/getGithubUrl")
+    public @ResponseBody String getGithubUrl(HttpServletRequest request) {
+        String reqUrl = githubApiUrl + "?client_id=" + clientId + "&redirect_uri=" + githubRedirectUrl;
+        return reqUrl;
+    }
+
     @GetMapping("/login/oauth2")
     public ResponseEntity main(@AuthenticationPrincipal OAuth2User oAuth2User, HttpSession session) throws IOException {
         System.out.println("userController");
