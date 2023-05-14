5ea3e516541c464016c5bc80722303840c387957
ehgmlKim <dohee930@gmail.com>
2023-05-15 00:05:03 +0900

    Feat: JWT 인증 토큰 미완

---
+++ src/main/java/com/proj/letsgitit/config/SecurityConfig.java
@@ -0,0 +1,44 @@
+package com.proj.letsgitit.config;
+
+import com.proj.letsgitit.config.jwt.JwtAuthenticationFilter;
+import com.proj.letsgitit.config.jwt.JwtTokenProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity(debug = true)
+public class SecurityConfig {
+    private final JwtTokenProvider jwtTokenProvider;
+
+    public SecurityConfig(JwtTokenProvider jwtTokenProvider) {
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    @Bean
+    public BCryptPasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+                .and()
+                .formLogin().disable()
+                .httpBasic().disable()
+
+                .authorizeRequests()
+                //이거 우리꺼로 수정해야함
+                .antMatchers ("/api/**", "/login/**", "/oauth2/**").permitAll ()
+                .and()
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+}
