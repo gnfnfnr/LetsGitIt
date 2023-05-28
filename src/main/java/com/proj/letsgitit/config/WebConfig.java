e497b06fe55816e0b2492ae246d33905f20b363e
ehgmlKim <dohee930@gmail.com>
2023-05-28 23:18:14 +0900

    Fix: 포트 번호 변경

--- src/main/java/com/proj/letsgitit/config/WebConfig.java
+++ src/main/java/com/proj/letsgitit/config/WebConfig.java
@@ -1,7 +1,6 @@
 package com.proj.letsgitit.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,6 +9,6 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000");
+                .allowedOrigins("http://localhost:9000");
     }
 }
